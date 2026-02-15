296238c0e725196e6ba9ad650cd7e43271de0d4c
Abdullah <iamabd979@gmail.com>
2026-02-16 01:59:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -210,4 +210,6 @@ def history():
     return render_template('history.html', expenses=expenses)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
