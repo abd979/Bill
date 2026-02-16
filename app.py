85b4d41d1d3b40b7da12606f575249221f0c4b93
Abdullah <iamabd979@gmail.com>
2026-02-16 18:21:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -447,4 +447,6 @@ def history():
                           user_filter=user_filter, status_filter=status_filter)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
